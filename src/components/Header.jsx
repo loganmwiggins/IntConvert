f6012358a03a86ba8ecb326062becf4f3ab50092
loganmwiggins <loganmwiggins1@gmail.com>
2025-10-05 20:40:54 -0400

    improved header navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 import '../stylesheets/ss-components/Header.css';
 
 function Header() {
     const navigate = useNavigate();
+    const location = useLocation();
+
+    const handleTitleClick = () => {
+        if (!location.pathname.includes('/home') && location.pathname !== '/') {
+            navigate('/');
+        }
+    };
+
+    const handleInfoClick = () => {
+        if (!location.pathname.includes('/info')) {
+            navigate('/info');
+        }
+    };
+
     return (
         <div className="header">
-            <div className="header-title">
+            <motion.div
+                className="header-title"
+                onClick={handleTitleClick}
+                whileTap={{ scale: 0.9 }}
+            >
                 <img src="/assets/icons/logo-v2.png" draggable="false" />
                 <h2>IntConvert</h2>
-            </div>
+            </motion.div>
             <div className="header-btns">
-                <button className="btn-icon" onClick={() => navigate('/info')}>
+                <motion.button className="btn-icon" onClick={handleInfoClick} whileTap={{ scale: 0.9 }}>
                     <img src="/assets/icons/info.svg" draggable="false" />
-                </button>
+                </motion.button>
             </div>
         </div>
     );

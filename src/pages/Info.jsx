174b098b3e04f81a418fa15e2c6d2e9a0586519a
loganmwiggins <loganmwiggins1@gmail.com>
2025-10-11 00:14:56 -0400

    GitHub link

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -5,8 +5,13 @@ import Card from '../components/Card';
 import '../stylesheets/Info.css';
 
 function Info() {
+    const handleGithubClick = () => {
+        window.open('https://github.com/loganmwiggins/int-convert', '_blank');
+    };
+
     return (
         <div className="page-wrapper">
+            {/* App Information */}
             <motion.div
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
@@ -15,11 +20,16 @@ function Info() {
             >
                 <Card header="App Information" canCollapse={false}>
                     <p>
-                        IntConvert was developed with the goal of creating a simple, intuitive, and well-designed application for converting integers with different bases. As a result, with this app, users are able to easily make accurate conversions between the binary, octal, decimal, and hexadecimal number systems.
+                        IntConvert was developed with the goal of creating a simple, 
+                        intuitive, and well-designed application for converting integers 
+                        with different bases. As a result, with this app, users are able 
+                        to easily make accurate conversions between the binary, octal, 
+                        decimal, and hexadecimal number systems.
                     </p>  
                 </Card>
             </motion.div>
             
+            {/* App Version */}
             <motion.div
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
@@ -29,6 +39,19 @@ function Info() {
                 <Card header="App Version" canCollapse={false}>
                     <p>Version 2.0.0</p>
                 </Card>
+            </motion.div>
+
+            {/* GitHub */}
+            <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 20 }}
+                transition={{ duration: 0.3, ease: "easeInOut", delay: 0.2 }}
+            >
+                <button type="button" className="btn-primary" onClick={handleGithubClick}>
+                    <img src="/assets/icons/github.svg" draggable="false" />
+                    <span>GitHub</span>
+                </button>
             </motion.div>
         </div>
     );
